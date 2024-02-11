5293659669c5acb60823f5d8069468c18b275857
Luca-Brabender <luca@brabenders.de>
2024-02-11 18:33:01 +0100

    Initial Commit

---
+++ RandomPic/RandomPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPicApp.swift
+//  RandomPic
+//
+//  Created by Luca Brabender on 11.02.24.
+//
+
+import SwiftUI
+
+@main
+struct RandomPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
